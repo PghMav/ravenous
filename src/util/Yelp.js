0f23311d5868db42466a8528004bb2e1800465b9
Bret Mavrich <bret@Bret-Mavrich-MacBook-Pro.local>
2019-08-16 19:54:36 -0400

    Parts 1, 2, & 3

---
+++ src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = 'DkXnlClRjXns0GdN0ksAUt9JN-FtD1HYCKnx3EoXI5HXJyZVjTEq86sUxhmtwuNXlv3LyiNJ6H0DPImaSZMhFPRIq5Ljj-8TPZQ7i4eBn1M-_94zj0FPcVeOAtkdXXYx';
+
+const Yelp = {
+  search(term,location,sortBy){
+    return fetch();
+  }
+};
