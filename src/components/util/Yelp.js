ee44c41a1d035b6aaac904109385d6d49dd398cd
Bret Mavrich <bret@Bret-Mavrich-MacBook-Pro.local>
2019-08-17 11:08:15 -0400

    Added links to images and addresses.

--- src/components/util/Yelp.js
+++ src/components/util/Yelp.js
@@ -15,6 +15,13 @@ export const Yelp = {
          //console.log(jsonResponse);
          return jsonResponse.businesses.map(business=>{
           //console.log(business);
+          const gMaps = 'https://www.google.com/maps/search/?api=1';
+          const gMapsUrl = (business) =>{
+            const gAddress = business["location"]["address1"].split(" ").join("+");
+            const gCity = business["location"]["city"];
+            const gState = business["location"]["state"]
+            return `${gMaps}&query=${gAddress}+${gCity}%2C${gState}`;
+          };
           return {
             id: business['id'],
             imgSrc: business["image_url"],
@@ -25,7 +32,9 @@ export const Yelp = {
             zipCode: business["location"]["zip_code"],
             category: business["categories"][0]["title"],
             rating: business["rating"],
-            reviewCount: business["review_count"]
+            reviewCount: business["review_count"],
+            url: business['url'],
+            gMapsUrl: gMapsUrl(business)
           };
         });
       //  throw new Error('Not working');
